8e795d59c5cf88f142cc918a1b1acaf7e82f4ab2
SoT03 <bogdan.zygadlo123@gmail.com>
2023-11-03 00:42:32 +0100

    Finished header structure

--- components/header.tsx
+++ components/header.tsx
@@ -3,10 +3,11 @@ import classes from '../styles/header.module.scss';
 const Header = () => {
 	return (
 		<header className={classes.header}>
-            <div className="bg-shadow"></div>
+			<div className='bg-shadow'></div>
 			<div className='wrapper'>
 				<h1>Track and trade</h1>
 				<h2>crypto currencies</h2>
+				<button>Get started</button>
 			</div>
 		</header>
 	);
