20564e4f99b8df47b2fdca55d10d55a1b89b1f00
SoT03 <bogdan.zygadlo123@gmail.com>
2023-11-07 13:06:13 +0100

    added currencies imges

--- components/marketTableItem.tsx
+++ components/marketTableItem.tsx
@@ -6,10 +6,16 @@ const TableItem: React.FC<{ data: tableCellsData; index: number }> = (
 	return (
 		<tr>
 			<td data-cell='id'>{props.index}</td>
-			<td data-cell='name'>{props.data.name}</td>
+			<td data-cell='name'>
+				<img
+					src={`https://s2.coinmarketcap.com/static/img/coins/64x64/${props.data.id}.png`}
+					alt=''
+				/>
+				{props.data.name}
+			</td>
 			<td data-cell='price'>{props.data.quote.USD.price.toFixed(2)} $</td>
 			<td data-cell='24 change'>
-				{props.data.quote.USD.percent_change_24h.toFixed(2)}
+				{props.data.quote.USD.percent_change_24h.toFixed(2)} %
 			</td>
 			<td data-cell='market cap'>
 				{props.data.quote.USD.market_cap.toFixed()}
