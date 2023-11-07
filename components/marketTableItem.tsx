22b68d19504deac40c7d0e0f56cf28540e0adb71
SoT03 <bogdan.zygadlo123@gmail.com>
2023-11-07 13:19:42 +0100

    finished table

--- components/marketTableItem.tsx
+++ components/marketTableItem.tsx
@@ -3,6 +3,9 @@ import { tableCellsData } from './market';
 const TableItem: React.FC<{ data: tableCellsData; index: number }> = (
 	props
 ) => {
+	const precentChange24: any =
+		props.data.quote.USD.percent_change_24h.toFixed(2);
+
 	return (
 		<tr>
 			<td data-cell='id'>{props.index}</td>
@@ -14,8 +17,10 @@ const TableItem: React.FC<{ data: tableCellsData; index: number }> = (
 				{props.data.name}
 			</td>
 			<td data-cell='price'>{props.data.quote.USD.price.toFixed(2)} $</td>
-			<td data-cell='24 change'>
-				{props.data.quote.USD.percent_change_24h.toFixed(2)} %
+			<td
+				data-cell='24 change'
+				className={precentChange24 >= 0 ? 'green-text' : 'red-text'}>
+				{precentChange24} %
 			</td>
 			<td data-cell='market cap'>
 				{props.data.quote.USD.market_cap.toFixed()}
