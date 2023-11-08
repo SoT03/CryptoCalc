ea5ff5375957a65387b541f145f20728130723ff
SoT03 <bogdan.zygadlo123@gmail.com>
2023-11-08 22:46:09 +0100

    created working card component for whyUs section

--- components/whyUs.tsx
+++ components/whyUs.tsx
@@ -1,12 +1,30 @@
 import classes from '../styles/aboutUs/whyUs.module.scss';
+import { IconThumbUp } from '@tabler/icons-react';
 import WhyUsCard from './whyUsCard';
+
+const cardData = [
+	{
+		title: 'Clients satisfaction',
+		message:
+			'Over 50,000 satisfied customers whom we have helped to successfully invest their money',
+		icon: IconThumbUp,
+	},
+];
 
 const WhyUs = () => {
 	return (
 		<section className={`section ${classes['whyUs-hero']}`}>
 			<h2>Why should you join us ?</h2>
 			<div className='wrapper'>
-				<WhyUsCard />
+				<div className={classes['card-box']}>
+					{cardData.map((item) => (
+						<WhyUsCard
+							title={item.title}
+							message={item.message}
+							icon={<item.icon />}
+						/>
+					))}
+				</div>
 			</div>
 		</section>
 	);
--- components/whyUsCard.tsx
+++ components/whyUsCard.tsx
@@ -1,5 +1,19 @@
-const WhyUsCard = () => {
-	return <h1>Hi</h1>;
+import { TablerIconsProps } from '@tabler/icons-react';
+
+type WhyUsCardType = {
+	title: string;
+	message: string;
+	icon: JSX.Element;
+};
+
+const WhyUsCard: React.FC<WhyUsCardType> = (props) => {
+	return (
+		<div className='card'>
+			{props.icon}
+			<h3>{props.title}</h3>
+			<p>{props.message}</p>
+		</div>
+	);
 };
 
 export default WhyUsCard;
