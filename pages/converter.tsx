060e23af015f4d001b59fe5ce434c8411e61f1ee
SoT03 <bogdan.zygadlo123@gmail.com>
2023-11-11 19:40:42 +0100

    styled convert site

--- pages/converter.tsx
+++ pages/converter.tsx
@@ -1,38 +1,46 @@
 import Layout from '@/components/layout/layout';
+import { IconArrowsLeftRight } from '@tabler/icons-react';
 
 import classes from '../styles/converter/converter.module.scss';
 
 const ConverterPage = () => {
+	const currenciesList = (
+		<select id='currency-one' className={classes['calc__box-select']}>
+			<option value='PLN' selected>
+				PLN
+			</option>
+			<option value='USD'>USD</option>
+			<option value='GBP'>GBP</option>
+			<option value='EUR'>EUR</option>
+			<option value='Crypto Currencies' disabled>
+				Crypto Currencies
+			</option>
+			<option value='USD'>BTC</option>
+			<option value='GBP'>BTC</option>
+			<option value='EUR'>BTC</option>
+			<option value='USD'>BTC</option>
+			<option value='GBP'>BTC</option>
+			<option value='EUR'>BTC</option>
+		</select>
+	);
+
 	return (
 		<Layout>
 			<main className={classes.calculator}>
 				<div className='wrapper'>
-					<div>
-						<div>
-							<input type='number' value='1' />
-							<select id='currency-one'>
-								<option value='PLN' selected>
-									PLN
-								</option>
-								<option value='USD'>USD</option>
-								<option value='GBP'>GBP</option>
-								<option value='EUR'>EUR</option>
-								<option value='CHF'>CHF</option>
-							</select>
-						</div>
-						<button></button>
+					<div className={classes.calc}>
+						<input
+							type='number'
+							value='1'
+							className={classes['calc__amount']}
+						/>
+						<div className={classes['calc__box']}>
+							{currenciesList}
+							<button className={classes['calc__box-btn']}>
+								<IconArrowsLeftRight />
+							</button>
 
-						<div>
-							<input type='number' disabled />
-							<select id='currency-two'>
-								<option value='PLN'>PLN</option>
-								<option value='USD' selected>
-									USD
-								</option>
-								<option value='GBP'>GBP</option>
-								<option value='EUR'>EUR</option>
-								<option value='CHF'>CHF</option>
-							</select>
+							<div>{currenciesList}</div>
 						</div>
 					</div>
 					<p></p>
