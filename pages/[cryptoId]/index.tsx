e9fd2d0bd376592190141322b518cc18afb6a261
SoT03 <bogdan.zygadlo123@gmail.com>
2023-11-15 13:15:52 +0100

    added fetching for each crypto

--- pages/[cryptoId]/index.tsx
+++ pages/[cryptoId]/index.tsx
@@ -1,6 +1,8 @@
 import { GetStaticProps } from 'next';
 
-const DetailPage: React.FC<{ cryptoId: string }> = (props) => {
+const DetailPage: React.FC<{ data: {}}> = (props) => {
+	console.log(props.data);
+
 	return <h1>H1</h1>;
 };
 
@@ -30,8 +32,25 @@ export const getStaticPaths = async () => {
 };
 
 export const getStaticProps = (async (context) => {
+	const cryptoId = context.params?.cryptoId;
+
+	const API_KEY = '4a3b5bb3-422d-41c7-8730-e219eff221e5';
+	const response = await fetch(
+		'https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=' + cryptoId,
+		{
+			method: 'GET',
+			headers: {
+				Accept: '*/*',
+				'X-CMC_PRO_API_KEY': API_KEY,
+			},
+		}
+	);
+	const data = await response.json();
+
+	const cryptoDetails = data.data;
+
 	return {
-		props: {},
+		props: { data: cryptoDetails },
 	};
 }) satisfies GetStaticProps;
 
